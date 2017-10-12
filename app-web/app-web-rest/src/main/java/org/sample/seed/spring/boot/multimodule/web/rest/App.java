edb1b25ee3f4d579462a4242e47d518b7b1a3812
c0demark <kumarvijay336@gmail.com>
2017-10-12 21:03:02 +0530

    added skeleton project structure

---
+++ app-web/app-web-rest/src/main/java/org/sample/seed/spring/boot/multimodule/web/rest/App.java
@@ -0,0 +1,15 @@
+package org.sample.seed.spring.boot.multimodule.web.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vijay Kumar
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
